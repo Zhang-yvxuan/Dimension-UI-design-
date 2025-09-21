18b641a897d9089d398094296ada4b25e4cde72b
Zhang-yvxuan <zsj8033@qq.com>
2025-09-21 14:19:45 +0800

    Delete 渡物dimension（UI设计） directory

--- 渡物dimension（UI设计）/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// 模拟用户数量增长
-const counterEl = document.getElementById("user-count");
-if(counterEl){
-  let count = 218374; // 初始基数
-  setInterval(()=>{
-    count += Math.floor(Math.random()*3); // 随机递增
-    counterEl.textContent = count.toLocaleString();
-  }, 1500);
-}
